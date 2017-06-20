877fa1d56658430fac69e291b5e27def71c4cfb1
Taymon A. Beal <taymon@google.com>
2017-06-20 16:01:52 -0400

    Remove StructuredProperty

--- static/comment.js
+++ static/comment.js
@@ -12,29 +12,29 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-$(document).ready(() => {
-  const commentForm = $('#comment-form');
-  const commentInputs = $('#comment-form :input');
-  const commentContent = $('#comment-content');
-  commentForm.submit((event) => {
+$(document).ready(function() {
+  var commentForm = $('#comment-form');
+  var commentInputs = $('#comment-form :input');
+  var commentContent = $('#comment-content');
+  commentForm.submit(function(event) {
     event.preventDefault();
-    const formData = commentForm.serialize();
+    var formData = commentForm.serialize();
     commentInputs.prop('disabled', true);
     $.post({url: '/submit-comment', data: formData, dataType: 'json'}).then(
-        (responseData) => {
+        function(responseData) {
           $('#comments')
               .append($('<div/>')
                   .append($('<h4/>')
                       .append($('<a/>', {
                         href: 'mailto:' + responseData.email,
-                        text: responseData.nickname,
+                        text: responseData.email,
                       })).append(document.createTextNode(' wrote:')))
                   .append($('<p/>', {text: commentContent.val()})));
           commentContent.val('');
           commentInputs.prop('disabled', false);
           commentContent.focus();
         },
-        () => {
+        function() {
           commentForm.append($('<p>Something went wrong.</p>'));
         });
   });
